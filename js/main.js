e297fe32b1a73f906e98a359f359e561af943451
Ezees <wertylaray@gmail.com>
2024-07-23 17:14:51 +0800

    Update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+AOS.init({
+    duration: 800,
+    easing: 'slide'
+});
+
+
 (function ($) {
     "use strict";
     /*=================================
@@ -38,6 +44,8 @@
     $(window).on("load", function () {
         $(".preloader").fadeOut();
     });
+
+  
 
     /*---------- 02. Preloader ----------*/
     if ($(".preloader").length > 0) {
@@ -896,6 +904,73 @@
         });
     });
     
+//  /*-------animet-------*/   
+
+var counter = function() {
+		
+    $('#section-counter, .hero-wrap, .ftco-counter, .ftco-about').waypoint( function( direction ) {
+
+        if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+
+            var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+            $('.number').each(function(){
+                var $this = $(this),
+                    num = $this.data('number');
+                    console.log(num);
+                $this.animateNumber(
+                  {
+                    number: num,
+                    numberStep: comma_separator_number_step
+                  }, 7000
+                );
+            });
+            
+        }
+
+    } , { offset: '95%' } );
+
+}
+counter();
+
+
+var contentWayPoint = function() {
+    var i = 0;
+    $('.ftco-animate').waypoint( function( direction ) {
+
+        if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+            
+            i++;
+
+            $(this.element).addClass('item-animate');
+            setTimeout(function(){
+
+                $('body .ftco-animate.item-animate').each(function(k){
+                    var el = $(this);
+                    setTimeout( function () {
+                        var effect = el.data('animate-effect');
+                        if ( effect === 'fadeIn') {
+                            el.addClass('fadeIn ftco-animated');
+                        } else if ( effect === 'fadeInLeft') {
+                            el.addClass('fadeInLeft ftco-animated');
+                        } else if ( effect === 'fadeInRight') {
+                            el.addClass('fadeInRight ftco-animated');
+                        } else {
+                            el.addClass('fadeInUp ftco-animated');
+                        }
+                        el.removeClass('item-animate');
+                    },  k * 50, 'easeInOutExpo' );
+                });
+                
+            }, 100);
+            
+        }
+
+    } , { offset: '95%' } );
+};
+contentWayPoint();
+
+//  /*-------magnific popup */
+
     // /*----------- 00. Right Click Disable ----------*/
     //   window.addEventListener('contextmenu', function (e) {
     //     // do something here...
