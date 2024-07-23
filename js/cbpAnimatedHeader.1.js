f015c033556922aed5e20adb1baaf4aff3405b7c
Ezees <wertylaray@gmail.com>
2024-07-23 23:45:55 +0800

    Update index

---
+++ js/cbpAnimatedHeader.1.js
@@ -0,0 +1,12 @@
+/**
+ * cbpAnimatedHeader.min.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+var cbpAnimatedHeader = (({
+    var: b = document.documentElement, g = document.querySelector(".cbp-af-header"), e = false, a = 300 }) => { });
